d2cb08b71da373b0f9ddbd0a08aa953e4c707a88
Roman Gajda <romanbg80@gmail.com>
2020-06-01 07:56:59 -0400

    basic buttons

--- app.py
+++ app.py
@@ -8,8 +8,16 @@ class MainWindow(tk.Tk):
         self.title("Mineralogical basis")
 
         ttk.Label(text="Baza mineralogiczna projektu Opus", padding=(30,10)).pack()
+        option_button = ttk.Button(text="Dostepne opcje", command=self.options)
+        option_button.pack(side="left")
+
+        quit_button = ttk.Button(text="Wyjdź z aplikacji", command=self.destroy)
+        quit_button.pack(side="left")
 
         DatabaseContentFrame(self).pack()
+
+    def options(self):
+        print("przycisk podłączony do funkcji")
 
 
 class DatabaseContentFrame(ttk.Frame):
