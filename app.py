5f8119a3246b10860bc262e726cc5180e6ed6176
Roman Gajda <romanbg80@gmail.com>
2020-06-04 13:15:49 -0400

    klasy i ramki uporządkowanie 1

--- app.py
+++ app.py
@@ -4,64 +4,58 @@ from PIL import Image, ImageTk      # inserting pictures
 
 
 class MineralogicalBasis(tk.Tk):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.title("Mineralogical basis")
-        MainWindow(self).grid()
+
+        frame = MainWindow(self, padding=(60, 30))
+        frame.grid()
 
 
 class MainWindow(ttk.Frame):
-    def __init__(self, container):
-        super().__init__(container)
-
-
-        headline_frame = ttk.Frame(padding=(20, 10, 20, 10))
-        headline_frame.grid(row=0, column=0)
-        headline = ttk.Label(headline_frame, text="Baza mineralogiczna projektu Opus")
-        headline.grid(row=0, column=0)
-
-        #DatabaseContentFrame(self).grid()
-
-        input_frame = ttk.Frame(padding=(20, 10, 20, 0))
-        input_frame.grid(row=1, column=0)
-
-        login_button = ttk.Button(input_frame, text="Login", command=self.login)
-        login_button.grid(row=1, column=4)
+    def __init__(self, container, **kwargs):
+        super().__init__(container, **kwargs)
 
         self.username = tk.StringVar()
         self.userpassword = tk.StringVar()
 
-        username_label = ttk.Label(input_frame, text="Username: ")
-        username_label.grid(row=1, column=0, padx=(0, 10))
-        username_entry = ttk.Entry(input_frame, width=15, textvariable=self.username)
-        username_entry.grid(row=1, column=1)
+        headline = ttk.Label(self, text="Baza mineralogiczna projektu Opus")
+        username_label = ttk.Label(self, text="Username: ")
+        username_entry = ttk.Entry(self, width=15, textvariable=self.username)
+        userpassword_label = ttk.Label(self, text="User Password: ")
+        userpassword_entry = ttk.Entry(self, width=15, textvariable=self.userpassword)
+        login_button = ttk.Button(self, text="Login", command=self.login)
+        option_button = ttk.Button(self, text="Dostepne opcje", command=self.options)
+        quit_button = ttk.Button(self, text="Wyjdź z aplikacji", command=self.destroy)
+
+        image = ttk.Label(self)
+
+        headline.grid(column=0, row=0, )
+        username_label.grid(column=0, row=1, sticky="W")
+        username_entry.grid(column=1, row=1, sticky="EW")
         username_entry.focus()
-
-        userpassword_label = ttk.Label(input_frame, text="User Password: ")
-        userpassword_label.grid(row=1, column=2, padx=(0, 10))
-        userpassword_entry = ttk.Entry(input_frame, width=15, textvariable=self.userpassword)
-        userpassword_entry.grid(row=1, column=3)
-
-        buttons_frame = ttk.Frame(padding=(20, 10))
-        buttons_frame.grid(row=2, column=0)
-
-        option_button = ttk.Button(buttons_frame, text="Dostepne opcje", command=self.options)
-        option_button.grid(row=2, column=0)
-
-        quit_button = ttk.Button(buttons_frame, text="Wyjdź z aplikacji", command=self.destroy)
-        quit_button.grid(row=2, column=1)
+        userpassword_label.grid(column=2, row=1, sticky="W")
+        userpassword_entry.grid(column=3, row=1, sticky="EW")
+        userpassword_entry.focus()
+        login_button.grid(column=4, row=1)
+        option_button.grid(column=0, row=2)
+        quit_button.grid(column=1, row=2)
 
         # jeszcze nie działa jak powinno
-        image_frame = ttk.Frame(padding=(20, 10))
-        image_frame.grid(row=3, column=0)
+        image.grid(row=3, column=5)
         image_1 = Image.open('znaczki.jpg')
         photo_1 = ImageTk.PhotoImage(image_1)
-        label = ttk.Label(image_frame, image=photo_1, padding=5)
-        label.grid(row=3, column=0)
+        label = ttk.Label(image, image=photo_1, padding=5)
+        label.grid(row=3, column=5)
 
-    def login(self, username):
+        for child in self.winfo_children():
+            child.grid_configure(padx=15, pady=15)
 
-        print(f"Użytkownik, {username.get()  or 'trzeba dodać'}")
+    def login(self, *args):
+        try:
+            print(f"Użytkownik, {self.username.get() or 'trzeba dodać'}")
+        except ValueError:
+            pass
 
     def options(self):
         print("przycisk podłączony do funkcji")
