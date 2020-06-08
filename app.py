0cdda6e84f60fd7c0bd80ff1acbe88d4418151ef
Roman Gajda <romanbg80@gmail.com>
2020-06-08 15:39:52 -0400

    dodanie nowych opcji

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from tkinter import ttk             # set of widgets
-from PIL import Image, ImageTk      # inserting pictures
+from tkinter import ttk                             # set of widgets
+from PIL import Image, ImageTk                      # inserting pictures
+from config.ConnectSQL import ConnectionConfig      # connect to database
 
 
 class MineralogicalBasis(tk.Tk):
@@ -12,16 +13,7 @@ class MineralogicalBasis(tk.Tk):
         container = ttk.Frame(self)
         container.grid(padx=60, pady=30, sticky="EW")
 
-        #main_frame = MainWindow(container, self)
-        #main_frame.grid(row=0, column=0, sticky="NSEW")
-
-        #options_to_main = Options(container, self)
-        #options_to_main.grid(row=0, column=0, sticky="NSEW")
-
-        #self.frames[MainWindow] = main_frame
-        #self.frames[Options] = options_to_main
-
-        for FrameClass in (MainWindow, Options):
+        for FrameClass in (MainWindow, Options, ShowDatabase):
             frame = FrameClass(container, self)
             self.frames[FrameClass] = frame
             frame.grid(row=0, column=0, sticky="NSEW")
@@ -50,7 +42,8 @@ class MainWindow(ttk.Frame):
         userpassword_label = ttk.Label(self, text="User Password: ")
         userpassword_entry = ttk.Entry(self, width=15, textvariable=self.userpassword)
         login_button = ttk.Button(self, text="Login", command=self.login)
-        option_button = ttk.Button(self, text="Dostepne opcje", command=lambda: controller.show_frame(Options))
+        option_button = ttk.Button(self, text="Dodaj nowy wpis", command=lambda: controller.show_frame(Options))
+        database_button = ttk.Button(self, text="Lista minerałów", command=lambda: controller.show_frame(ShowDatabase))
         quit_button = ttk.Button(self, text="Wyjdź z aplikacji", command=self.destroy)
 
         image = ttk.Label(self)
@@ -64,7 +57,8 @@ class MainWindow(ttk.Frame):
         userpassword_entry.focus()
         login_button.grid(column=4, row=1)
         option_button.grid(column=0, row=2)
-        quit_button.grid(column=1, row=2)
+        database_button.grid(column=1, row=2)
+        quit_button.grid(column=2, row=2)
 
         # jeszcze nie działa jak powinno
         image.grid(row=3, column=5)
@@ -89,16 +83,101 @@ class MainWindow(ttk.Frame):
 class Options(ttk.Frame):
     def __init__(self, container, controller, **kwargs):
         super().__init__(container, **kwargs)
+        self.conn = ConnectionConfig().connection()
+        self.c = self.conn.cursor()
 
-        option1_label = ttk.Label(self, text="Opcja numer 1")
-        option1_button = ttk.Button(self, text="Wykonaj opcje 1")
-        option1_label.grid(column=0, row=0, sticky="W")
-        option1_button.grid(column=1, row=0, sticky="EW")
+        self.nazwa_pol = tk.StringVar()
+        self.nazwa_ang = tk.StringVar()
+        self.formula = tk.StringVar()
+        self.group = tk.StringVar()
+        self.system = tk.StringVar()
+        self.a_axis = tk.StringVar()
+        self.b_axis = tk.StringVar()
+        self.c_axis = tk.StringVar()
+        self.alpha = tk.StringVar()
+        self.beta = tk.StringVar()
+        self.gamma = tk.StringVar()
+        self.z_number = tk.StringVar()
+        self.code = tk.StringVar()
 
-        to_main_label = ttk.Label(self, text="Powrót do strony głównej")
-        to_main_button = ttk.Button(self, text="Powróć", command=lambda: controller.show_frame(MainWindow))
-        to_main_label.grid(column=0, row=1, sticky="W")
-        to_main_button.grid(column=0, row=1, sticky="EW")
+        name_pol_label = ttk.Label(self, width=20, text="nazwa polska")
+        name_ang_label = ttk.Label(self, width=20, text="nazwa angielska")
+        formula_label = ttk.Label(self, width=20, text="formuła")
+        group_label = ttk.Label(self, width=20, text='grupa przestrzenna')
+        system_label = ttk.Label(self, width=20, text='układ krystalograficzny')
+        a_label = ttk.Label(self, width=20, text="a")
+        b_label = ttk.Label(self, width=20, text="b")
+        c_label = ttk.Label(self, width=20, text="c")
+        alpha_label = ttk.Label(self, width=20, text="alpha")
+        beta_label = ttk.Label(self, width=20, text="beta")
+        gamma_label = ttk.Label(self, width=20, text="gamma")
+        z_number_label = ttk.Label(self, width=20, text="Z number")
+        code_label = ttk.Label(self, width=20, text="code")
+
+        name_pol_entry = ttk.Entry(self, width=15, textvariable=self.nazwa_pol)
+        name_ang_entry = ttk.Entry(self, width=15, textvariable=self.nazwa_ang)
+        formula_entry = ttk.Entry(self, width=15, textvariable=self.formula)
+        group_entry = ttk.Entry(self, width=15, textvariable=self.group)
+        system_entry = ttk.Entry(self, width=15, textvariable=self.system)
+        a_entry = ttk.Entry(self, width=15, textvariable=self.a_axis)
+        b_entry = ttk.Entry(self, width=15, textvariable=self.b_axis)
+        c_entry = ttk.Entry(self, width=15, textvariable=self.c_axis)
+        alpha_entry = ttk.Entry(self, width=15, textvariable=self.alpha)
+        beta_entry = ttk.Entry(self, width=15, textvariable=self.beta)
+        gamma_entry = ttk.Entry(self, width=15, textvariable=self.gamma)
+        z_number_entry = ttk.Entry(self, width=15, textvariable=self.z_number)
+        code_entry = ttk.Entry(self, width=15, textvariable=self.code)
+
+        name_pol_label.grid(row=0, column=0)
+        name_ang_label.grid(row=0, column=1)
+        formula_label.grid(row=0, column=2)
+        group_label.grid(row=0, column=3)
+        system_label.grid(row=0, column=4)
+        name_pol_entry.grid(row=1, column=0)
+        name_ang_entry.grid(row=1, column=1)
+        formula_entry.grid(row=1, column=2)
+        group_entry.grid(row=1, column=3)
+        system_entry.grid(row=1, column=4)
+        a_label.grid(row=2, column=0)
+        b_label.grid(row=2, column=1)
+        c_label.grid(row=2, column=2)
+        alpha_label.grid(row=2, column=3)
+        beta_label.grid(row=2, column=4)
+        gamma_label.grid(row=2, column=5)
+        a_entry.grid(row=3, column=0)
+        b_entry.grid(row=3, column=1)
+        c_entry.grid(row=3, column=2)
+        alpha_entry.grid(row=3, column=3)
+        beta_entry.grid(row=3, column=4)
+        gamma_entry.grid(row=3, column=5)
+        z_number_label.grid(row=4, column=0)
+        code_label.grid(row=4, column=1)
+        z_number_entry.grid(row=5, column=0)
+        code_entry.grid(row=5, column=1)
+
+        option1_button = ttk.Button(self, text="Dodaj", command=self.addToDatabase)
+        option1_button.grid(column=0, row=6, sticky="EW")
+
+        to_main_button = ttk.Button(self, text="Powrót do strony głównej",
+                                    command=lambda: controller.show_frame(MainWindow))
+        to_main_button.grid(column=0, row=7, sticky="EW")
+
+    def addToDatabase(self):
+        print("Podłączony")
+        self.c.execute("INSERT INTO minerals_list_vials VALUES (default, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+                       (self.nazwa_pol.get(), self.nazwa_ang.get(), self.formula.get(), self.group.get(),
+                        self.system.get(), self.a_axis.get(), self.b_axis.get(), self.c_axis.get(),
+                        self.alpha.get(), self.beta.get(), self.gamma.get(), self.z_number.get(), self.code.get()))
+
+
+
+class ShowDatabase(ttk.Frame):
+    def __init__(self, container, controller, **kwargs):
+        super().__init__(container, **kwargs)
+
+        to_main_button = ttk.Button(self, text="Powrót do strony głównej",
+                                    command=lambda: controller.show_frame(MainWindow))
+        to_main_button.grid(column=1, row=1, sticky="EW")
 
 
 root = MineralogicalBasis()
--- controller/basis_controller.py
+++ controller/basis_controller.py
@@ -8,23 +8,24 @@ class BasisManager:
 
     def tableCreator(self):
         # wykonanie zapytania
-        self.c.execute("""CREATE TABLE minerals_list_vials (
-            id smallint(6) NOT NULL,
-            name_pol varchar(32) NOT NULL,
-            name_ang varchar(32) NOT NULL,
-            formula varchar(64) NOT NULL,
-            crystal_system varchar(16) DEFAULT NULL,
-            space_group varchar(16) DEFAULT NULL,
-            a_axis decimal(8,4) DEFAULT NULL,
-            b_axis decimal(8,4) DEFAULT NULL,
-            c_axis decimal(8,4) DEFAULT NULL,
-            alpha decimal(8,4) DEFAULT NULL,
-            beta decimal(8,4) DEFAULT NULL,
-            gamma decimal(8,4) DEFAULT NULL,
-            Z_number smallint(6) DEFAULT NULL,
-            code varchar(16) NOT NULL,
-            PRIMARY KEY (id)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8""")
+        #self.c.execute("DROP TABLE minerals_list_vials")
+        self.c.execute("CREATE TABLE minerals_list_vials ("
+            "id int auto_increment, "
+            "name_pol varchar(32) NOT NULL, "
+            "name_ang varchar(32) NOT NULL, "
+            "formula varchar(64) NOT NULL, "
+            "crystal_system varchar(16) DEFAULT NULL, "
+            "space_group varchar(16) DEFAULT NULL, "
+            "a_axis decimal(8,4) DEFAULT NULL, "
+            "b_axis decimal(8,4) DEFAULT NULL, "
+            "c_axis decimal(8,4) DEFAULT NULL, "
+            "alpha decimal(8,4) DEFAULT NULL, "
+            "beta decimal(8,4) DEFAULT NULL, "
+            "gamma decimal(8,4) DEFAULT NULL, "
+            "Z_number smallint(6) DEFAULT NULL, "
+            "code varchar(16) NOT NULL, "
+            "PRIMARY KEY (id) "
+            ") ENGINE=InnoDB DEFAULT CHARSET=utf8")
         # pobranie wyniku zwracanego przez zapytanie
         result = self.c.fetchone()
 
